6581ea08f39558cce478c69bbf7edb4149c63f0a
Jay Patel <pateljay24004@gmail.com>
2023-01-14 02:28:33 -0800

    updated some transformer

--- utils/transformer.py
+++ utils/transformer.py
@@ -1,11 +1,24 @@
 import discord
-import re
 from discord import app_commands, Interaction
 from discord.ext import commands
-
+import re
 
 time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
 time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
+
+class TimeConverter(app_commands.Transformer):
+    async def transform(self, interaction: discord.Interaction, argument: str) -> int:
+        args = argument.lower()
+        matches = re.findall(time_regex, args)
+        time = 0
+        for key, value in matches:
+            try:
+                time += time_dict[value] * float(key)
+            except KeyError:
+                raise app_commands.BadArgument(f"{value} is an invalid time key! h|m|s|d are valid arguments")
+            except ValueError:
+                raise app_commands.BadArgument(f"{key} is not a number!")
+        return round(time)
 
 class MutipleRole(app_commands.Transformer):
     async def transform(self, interaction: Interaction, value: str,):
@@ -13,28 +26,9 @@ class MutipleRole(app_commands.Transformer):
         roles = [await commands.RoleConverter().convert(interaction, role) for role in value]
         return roles
 
-class MutipleUser(app_commands.Transformer):
+class MultipleMember(app_commands.Transformer):
     async def transform(self, interaction: Interaction, value: str,):
         value = value.split(" ")
-        users = [await commands.UserConverter().convert(interaction, user) for user in value]
-        return users
-
-class MutipleMember(app_commands.Transformer):
-    async def transform(self, interaction: Interaction, value: str,):
-        value = value.split(" ")
-        members = [await commands.MemberConverter().convert(interaction, member) for member in value]
+        value = [value.replace("<", "").replace(">", "").replace("@", "").replace("!", "") for value in value]
+        members = [interaction.guild.get_member(int(member)) for member in value if member is not None]
         return members
-
-class TimeConverter(app_commands.Transformer):
-    async def transform(self, interaction: Interaction, value: str,):
-        args = value.lower()
-        matches = re.findall(time_regex, args)
-        time = 0
-        for key, value in matches:
-            try:
-                time += time_dict[value] * float(key)
-            except KeyError:
-                return await interaction.response.send_message(f"{value} is an invalid time key! h|m|s|d are valid arguments", ephemeral=True)                
-            except ValueError:
-                return await interaction.response.send_message(f"{key} is not a number!", ephemeral=True)
-        return round(time)
