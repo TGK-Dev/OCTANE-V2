a57067fed2aff9a10f9796896f471d648c3c88da
Jay Patel <pateljay24004@gmail.com>
2023-02-05 18:38:05 +0530

    added join gate

---
+++ cogs/serversettings.py
@@ -0,0 +1,184 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.app_commands import Group
+from typing import Literal
+from utils.db import Document
+from utils.views.JoinGateSettings_system import JoinGateSettings_Edit
+import unicodedata
+import unidecode
+import stringcase
+import re
+import datetime
+
+class serversettingsDB():
+    def __init__(self, bot, Document):
+        self.bot = bot
+        self.join_gate = Document(bot.db, "join_gate")
+        self.starboard = Document(bot.db, "starboard")
+    
+    async def get_config(self, settings, guild_id):
+        match settings:
+            case "join_gate":
+                config = await self.join_gate.find(guild_id)
+                if config is None: config = await self.create_config(guild_id)
+                return config
+            case "starboard":
+                config = await self.starboard.find(guild_id)
+                if config is None: config = await self.create_config(guild_id)
+                return config
+    
+    async def create_config(self, perk, guild_id):
+        match perk:
+            case "join_gate":
+                config = {"_id": guild_id, "joingate": {'enabled': None, 'action': None, 'accountage': None, 'whitelist': [], 'autorole': [], 'decancer': None, 'logchannel': None}}
+                await self.join_gate.insert(config)
+                return config
+
+
+    async def update_config(self, settings, guild_id, config):
+        match settings:
+            case "join_gate":
+                await self.join_gate.update(guild_id, config)
+
+class serversettings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.ss = serversettingsDB(bot, Document)
+    
+    @app_commands.command(name="serversettings", description="Change the settings of the server")
+    @app_commands.choices(settings=[app_commands.Choice(name="Join Gate", value="join_gate")])
+    @app_commands.describe(option="Show or edit the settings", settings="The settings you want to change")
+    @app_commands.default_permissions(administrator=True)
+    async def serversettings(self, interaction: discord.Interaction, settings: app_commands.Choice[str], option: Literal['Show', 'Edit']):
+        match settings.value:
+            case "join_gate":
+                config = await self.bot.ss.get_config(settings.value, interaction.guild_id)
+                embed = discord.Embed(title="Join Gate Settings", description="", color=0x363940)
+                embed.description += f"**Enabled:** `{config['joingate']['enabled']}`\n"
+                embed.description += f"**Decancer:** `{config['joingate']['decancer'] if config['joingate']['decancer'] is not None else 'None'}`\n"
+                embed.description += f"**Action:** `{config['joingate']['action'] if config['joingate']['action'] is not None else 'None'}`\n"
+                embed.description += f"**Account Age:** `{str(config['joingate']['accountage']) +('Days') if config['joingate']['accountage'] is not None else 'None'}`\n"
+                embed.description += f"**Whitelist:** {','.join([f'<@{user}>' for user in config['joingate']['whitelist']]) if len(config['joingate']['whitelist']) > 0 else '`None`'}\n"
+                embed.description += f"**Auto Role:** {','.join([f'<@&{role}>' for role in config['joingate']['autorole']]) if len(config['joingate']['autorole']) > 0 else '`None`'}\n"
+                embed.description += f"**Log Channel:** {interaction.guild.get_channel(config['joingate']['logchannel']).mention if config['joingate']['logchannel'] is not None else '`None`'}\n"
+
+                if option == "Show":
+                    await interaction.response.send_message(embed=embed)
+                elif option == "Edit":
+                    view = JoinGateSettings_Edit(interaction, config)
+                    await interaction.response.send_message(embed=embed, view=view)
+                    view.message = await interaction.original_response()
+                    await view.wait()
+                    if view.value:
+                        await self.bot.ss.update_config(settings.value, interaction.guild_id, view.data)
+
+class JoinGateBackEnd(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.joingate_cache = {}
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        for guild in await self.bot.rr.join_gate.get_all():self.bot.joingate_cache[guild["_id"]] = guild["joingate"]
+    
+    @staticmethod
+    def is_cancerous(text: str) -> bool:
+        for segment in text.split():
+            for char in segment:
+                if not (char.isascii() and char.isalnum()):
+                    return True
+        return False
+
+    @staticmethod
+    def strip_accs(text):
+        try:
+            text = unicodedata.normalize("NFKC", text)
+            text = unicodedata.normalize("NFD", text)
+            text = unidecode.unidecode(text)
+            text = text.encode("ascii", "ignore")
+            text = text.decode("utf-8")
+        except Exception as e:
+            print(e)
+        return str(text)
+
+    async def nick_maker(self, guild: discord.Guild, old_shit_nick):
+        old_shit_nick = self.strip_accs(old_shit_nick)
+        new_cool_nick = re.sub("[^a-zA-Z0-9 \n.]", "", old_shit_nick)
+        new_cool_nick = " ".join(new_cool_nick.split())
+        new_cool_nick = stringcase.lowercase(new_cool_nick)
+        new_cool_nick = stringcase.titlecase(new_cool_nick)
+        default_name = "Request a new name"
+        if len(new_cool_nick.replace(" ", "")) <= 1 or len(new_cool_nick) > 32:
+            if default_name == "Request a new name":
+                new_cool_nick = await self.get_random_nick(2)
+            elif default_name:
+                new_cool_nick = default_name
+            else:
+                new_cool_nick = "Request a new name"
+        return new_cool_nick
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        if member.bot: return
+        if member.guild.id not in self.bot.joingate_cache.keys(): return
+        data = self.bot.joingate_cache[member.guild.id]
+        if not data["joingate"]["enabled"]: return
+        if member.id in data["joingate"]["whitelist"]: return
+
+        fail = self.bot.dispatch("joingate_check", member)
+        if fail: return
+
+        if data["joingate"]["decancer"]:
+            print('doing decancer')
+            if self.is_cancerous(member.display_name):
+                print('is cancerous')
+                new_nick = await self.nick_maker(member.guild, member.display_name)
+                
+                embed = discord.Embed(color=discord.Color.green(), title="Decancer", description="")
+                embed.description += f"**Offender:** {member.mention}\n"
+                embed.description += f"**Action:** Nickname Decancer\n"
+                embed.description += f"**Reason:** Nickname contained non-ascii characters\n"
+                embed.description += f"**Moderator:** {self.bot.user.mention}\n"
+                embed.set_footer(text=f"User ID: {member.id}")
+                embed.timestamp = datetime.datetime.utcnow()
+
+                #logchannel = member.guild.get_channel(data["joingate"]["logchannel"])
+                try:
+                    await member.edit(nick=new_nick, reason="joingate decancer")
+                except:
+                    return
+                #if logchannel: await logchannel.send(embed=embed)
+    
+    @commands.Cog.listener()
+    async def on_joingate_check(self, member: discord.Member):
+        data = self.bot.joingate_cache[member.guild.id]
+        guild = member.guild
+        if not data['joingate']['accountage']: return
+        if (discord.utils.utcnow() - member.created_at).days < int(data['joingate']['accountage']):
+            try:
+                await member.send(f"Your account is too new to join this server. Please wait {data['joingate']['accountage']} days before joining.")
+            except discord.Forbidden:
+                pass
+            if data["joingate"]["action"] == "kick":
+                await member.kick(reason="joingate kick")
+            elif data["joingate"]["action"] == "ban":
+                await member.ban(reason="joingate ban")
+            embed = discord.Embed(title="Kick", description="")
+            embed.description += f"**Target:** {member.mention}\n"
+            embed.description += f"**Action:** {data['joingate']['action'].title()}\n"
+            embed.description += f"**Reason:** Account age is less than joingate account age\n"
+            embed.description += f"**Moderator:** {self.bot.user.mention}\n"
+            embed.set_footer(text=f"ID: {member.id}")
+            embed.timestamp = discord.utils.utcnow()
+            embed.color = discord.Color.red()
+
+            logchannel = member.guild.get_channel(data["joingate"]["logchannel"])
+            if logchannel: await logchannel.send(embed=embed)
+            return True
+        else:
+            roles = [guild.get_role(role) for role in data["joingate"]["autorole"]]
+            await member.edit(roles=roles, reason="joingate autorole")
+
+async def setup(bot):
+    await bot.add_cog(serversettings(bot))
---
+++ utils/views/JoinGateSettings_system.py
@@ -0,0 +1,141 @@
+import discord
+from discord import Interaction, SelectOption
+from discord.ui import View, Button, button, TextInput, Item
+from .selects import Role_select, Select_General, Channel_select
+from .modal import General_Modal
+
+
+class JoinGateSettings_Edit(View):
+    def __init__(self, interaction: Interaction, data:dict, message: discord.Message=None):
+        self.interaction = interaction
+        self.message = message
+        self.data = data
+        self.value = False
+        super().__init__()
+    
+    def update_embed(self, data):
+        embed = discord.Embed(title="Join Gate Settings", description="", color=0x363940)
+        embed.description += f"**Enabled:** `{data['joingate']['enabled']}`\n"
+        embed.description += f"**Decancer:** `{data['joingate']['decancer'] if data['joingate']['decancer'] is not None else 'None'}`\n"
+        embed.description += f"**Action:** `{data['joingate']['action'] if data['joingate']['action'] is not None else 'None'}`\n"
+        embed.description += f"**Account Age:** `{str(data['joingate']['accountage']) +(' Days') if data['joingate']['accountage'] is not None else 'None'}`\n"
+        embed.description += f"**Whitelist:** {','.join([f'<@{user}>' for user in data['joingate']['whitelist']]) if len(data['joingate']['whitelist']) > 0 else '`None`'}\n"
+        embed.description += f"**Auto Role:** {','.join([f'<@&{role}>' for role in data['joingate']['autorole']]) if len(data['joingate']['autorole']) > 0 else '`None`'}\n"
+        embed.description += f"**Log Channel:** {self.interaction.guild.get_channel(data['joingate']['logchannel']).mention if data['joingate']['logchannel'] is not None else '`None`'}\n"
+
+        return embed
+
+    @button(label="Toggle", style=discord.ButtonStyle.gray, custom_id="joingate_toggle", row=0)
+    async def joingate_toggle(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        if self.data['joingate']['enabled'] == True:self.data['joingate']['enabled'] = False
+        else:self.data['joingate']['enabled'] = True
+        await interaction.response.edit_message(embed=self.update_embed(self.data), view=self)
+    
+    @button(label="Decancer", style=discord.ButtonStyle.gray, custom_id="joingate_decancer", row=0)
+    async def joingate_decancer(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        if self.data['joingate']['decancer'] == True:self.data['joingate']['decancer'] = False
+        else:self.data['joingate']['decancer'] = True
+        await interaction.response.edit_message(embed=self.update_embed(self.data), view=self)
+
+    @button(label="Action", style=discord.ButtonStyle.gray, custom_id="joingate_action", row=0)
+    async def joingate_action(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        view = View()
+        view.value = False
+        options = [SelectOption(label="Kick", value="kick"), SelectOption(label="Ban", value="ban")]
+        view.select = Select_General(placeholder="Please select to perform", options=options, max_values=1)
+        view.add_item(view.select)
+
+        await interaction.response.send_message(view=view, ephemeral=True)
+        await view.wait()
+        if view.value:
+            self.data['joingate']['action'] = view.select.values[0]
+            await interaction.message.edit(embed=self.update_embed(self.data), view=self)
+            await view.select.interaction.response.edit_message(view=None, embed=discord.Embed(description=f"Join Gate action has been updated to {view.select.values[0]}"))
+        else:
+            await interaction.delete_original_response()
+
+    @button(label="Account Age", style=discord.ButtonStyle.gray, custom_id="joingate_accountage", row=1)
+    async def joingate_accountage(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        view = View()
+        view.value = False
+        options = [SelectOption(label="1 Day", value="1"), SelectOption(label="2 Days", value="2"), SelectOption(label="3 Days", value="3"), SelectOption(label="4 Days", value="4"), SelectOption(label="5 Days", value="5"), SelectOption(label="6 Days", value="6"), SelectOption(label="7 Days", value="7")]
+        view.select = Select_General(placeholder="Please select account age", options=options, max_values=1)
+        view.add_item(view.select)
+
+        await interaction.response.send_message(view=view, ephemeral=True)
+        await view.wait()
+        if view.value:
+            self.data['joingate']['accountage'] = int(view.select.values[0])
+            await interaction.message.edit(embed=self.update_embed(self.data), view=self)
+            await view.select.interaction.response.edit_message(view=None, embed=discord.Embed(description=f"Join Gate account age has been updated to {view.select.values[0]} Days"))
+        else:
+            await interaction.delete_original_response()
+
+    @button(label="Whitelist", style=discord.ButtonStyle.gray, custom_id="joingate_whitelist", row=1)
+    async def joingate_whitelist(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        modal = General_Modal(title="Whitelist Users Form", interaction=interaction)
+        modal.qestion = TextInput(label="Whitelist Users", placeholder="Please enter user ids to whitelist", max_length=1000)
+        if self.data['joingate']['whitelist'] is not None:modal.qestion.default = ','.join(self.data['joingate']['whitelist'])
+        modal.add_item(modal.qestion)
+        await interaction.response.send_modal(modal)
+        await modal.wait()
+        if modal.value:
+            self.data['joingate']['whitelist']= []
+            for user in modal.qestion.value.split(','):
+                if int(user) not in self.data['joingate']['whitelist']:
+                    self.data['joingate']['whitelist'].append(int(user))
+            await modal.interaction.response.edit_message(embed=self.update_embed(self.data), view=self)
+        
+    @button(label="Auto Role", style=discord.ButtonStyle.gray, custom_id="joingate_autorole", row=1)
+    async def joingate_autorole(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        view = View()
+        view.value = False
+        view.select = Role_select(placeholder="Please select roles to add/remove", max_values=10, min_values=1)
+        view.add_item(view.select)
+
+        await interaction.response.send_message(view=view, ephemeral=True)
+        await view.wait()
+        if view.value:
+            add_roles = ""
+            remove_roles = ""
+            failed_roles = ""
+
+            for role in view.select.values:
+                if role.is_default(): failed_roles += f"{role.mention}, "; continue
+                if role.position + 1 > interaction.guild.me.top_role.position: failed_roles += f"{role.mention}, "; continue
+                if role.permissions.administrator or role.permissions.manage_guild or role.permissions.manage_roles or role.permissions.kick_members or role.permissions.ban_members: failed_roles += f"{role.mention}, "; continue
+            
+                if role.id in self.data['joingate']['autorole']:
+                    self.data['joingate']['autorole'].remove(role.id)
+                    remove_roles += f"{role.mention}, "
+                else:
+                    self.data['joingate']['autorole'].append(role.id)
+                    add_roles += f"{role.mention}, "
+            await view.select.interaction.response.edit_message(content=f"Added roles: {add_roles}\nRemoved roles: {remove_roles}\nFailed roles: {failed_roles}", view=None)
+            await interaction.message.edit(embed=self.update_embed(self.data), view=self)
+        else:
+            await interaction.delete_original_response()
+    
+    @button(label="Log Channel", style=discord.ButtonStyle.gray, custom_id="joingate_logchannel", row=1)
+    async def joingate_logchannel(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        view = View()
+        view.value = False
+        view.select = Channel_select(placeholder="Please select a channel", max_values=1, min_values=1, channel_types=[discord.ChannelType.text])
+        view.add_item(view.select)
+
+        await interaction.response.send_message(view=view, ephemeral=True)
+        await view.wait()
+        if view.value:
+            self.data['joingate']['logchannel'] = view.select.values[0].id
+            await interaction.message.edit(embed=self.update_embed(self.data), view=self)
+            await view.select.interaction.response.edit_message(view=None, embed=discord.Embed(description=f"Join Gate log channel has been updated to {view.select.values[0].mention}"))
+        else:
+            await interaction.delete_original_response()
+    
+    @button(label="Save", style=discord.ButtonStyle.gray, custom_id="joingate_save", row=2)
+    async def joingate_save(self,  interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = True
+        for button in self.children:button.disabled = True
+        await interaction.message.edit(view=self)
+        await interaction.response.send_message(content="Saved", ephemeral=True)
+        self.stop()
