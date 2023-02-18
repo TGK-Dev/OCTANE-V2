b3c0a51fbf3449a717039cc23a559d94fba68cc2
Jay Patel <pateljay24004@gmail.com>
2023-02-18 14:21:10 +0530

    added millify converter

--- utils/converters.py
+++ utils/converters.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import re
+import math
 
 time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
 time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
@@ -17,3 +18,11 @@ class TimeConverter(commands.Converter):
             except ValueError:
                 raise commands.BadArgument("{} is not a number!".format(v))
         return time
+
+def millify(n):
+    n = float(n)
+    millnames = ['',' Thousand',' Million',' Billion',' Trillion']
+    millidx = max(0,min(len(millnames)-1,
+                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
