02326c0a2f76934281b3098177479d8de3ba5710
Jay Patel <pateljay24004@gmail.com>
2023-02-19 01:39:15 +0530

    added error handler

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import asyncio
 import datetime
 import logging
 import logging.handlers
+import aiohttp
 
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -44,7 +45,7 @@ bot = Bot_base(998152864201457754, False)
 bot.secret = os.environ.get("SECRET")
 bot.token = os.environ.get("TOKEN")
 bot.connection_url = os.environ.get("MONGO")    
-
+tree = bot.tree
 async def main():
     await bot.start(bot.token)
 
@@ -60,5 +61,28 @@ async def on_ready():
 async def ping(interaction):
     await interaction.response.send_message("Pong!")
     await interaction.edit_original_response(content=None, embed=discord.Embed(description=f"Ping {bot.latency*1000.0:.2f}ms"))
+
+@tree.error
+async def on_app_command_error(interaction: discord.Interaction, error):
+    if isinstance(error, app_commands.errors.CommandOnCooldown):
+        return await interaction.response.send_message(f"Please wait {error.retry_after:.2f} seconds before trying again.", ephemeral=True, delete_after=10)
+    else:
+        embed = discord.Embed(description=f"```\n{error}\n```", color=bot.default_color)
+        try:
+            await interaction.response.send_message(embed=embed, ephemeral=True, delete_after=10)
+        except:
+            await interaction.followup.send(embed=embed, ephemeral=True, delete_after=10)
+        
+    url = "https://canary.discord.com/api/webhooks/1076590771542708285/88D3SRMTYHPe4copSvTQ451KXx7Tk2WDsRYUPN4wtVI1qQbqDmyn0eAiUOhV7XW8SO3G"
+    embed = discord.Embed(title="Error", description=f"```\n{error}\n```", color=bot.default_color)
+    embed.add_field(name="Interaction Data", value=f"```\n{interaction.data}\n```", inline=False)
+    embed.add_field(name="Channel", value=f"{interaction.channel.mention} | {interaction.channel.id}", inline=False)
+    embed.add_field(name="Guild", value=f"{interaction.guild.name} | {interaction.guild.id}", inline=False)
+    embed.add_field(name="Author", value=f"{interaction.user.mention} | {interaction.user.id}", inline=False)
+    embed.add_field(name="Command", value=f"{interaction.command.name}", inline=False)
+    
+    async with aiohttp.ClientSession() as session:
+        webhook = discord.Webhook.from_url(url, session=session)
+        await webhook.send(embed=embed, avatar_url=interaction.client.user.avatar.url if interaction.client.user.avatar else interaction.client.user.default_avatar, username=f"{interaction.client.user.name}'s Error Logger")
 
 asyncio.run(main())
