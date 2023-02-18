f91f529aa6676e489245959d75db29536d3d8676
Jay Patel <pateljay24004@gmail.com>
2023-02-19 01:39:23 +0530

    added event cog

---
+++ cogs/events.py
@@ -0,0 +1,126 @@
+
+import datetime
+import discord
+import asyncio
+import re
+
+from discord.ext import commands, tasks
+from copy import deepcopy
+from discord import app_commands
+from utils.db import Document
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.vote_remider_task = self.check_vote_reminders.start()
+        self.bot.votes = Document(self.bot.db, "votes")
+        self.vote_task_progress = False
+    
+    def cog_unload(self):
+        self.vote_remider_task.cancel()
+    
+    @tasks.loop(minutes=1)
+    async def check_vote_reminders(self):
+        if self.vote_task_progress:
+            return
+        self.vote_task_progress = True
+
+        current_time = datetime.datetime.utcnow()
+        current_data = await self.bot.votes.get_all()
+        for data in current_data:
+            if data["reminded"] == True: continue
+        
+            expired_time = data['last_vote'] + datetime.timedelta(hours=12)
+            if current_time >= expired_time and data["reminded"] == False:
+                self.bot.dispatch("vote_reminder", data)
+            
+            if expired_time > current_time + datetime.timedelta(days=30):
+                await self.bot.votes.delete(data['_id'])
+        
+        self.vote_task_progress = False
+    
+    @check_vote_reminders.before_loop
+    async def before_check_vote_reminders(self):
+        await self.bot.wait_until_ready()
+    
+    @commands.Cog.listener()
+    async def on_vote_reminder(self, data):
+        if data["reminded"] == True: return
+
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(label="Vote for TGK at Top.gg", emoji="<a:tgk_icon:1002504426172448828>",url="https://top.gg/servers/785839283847954433/vote"))
+
+        guild = self.bot.get_guild(785839283847954433)
+        member = guild.get_member(data['_id'])
+        if member is None:
+            return await self.bot.votes.delete(data['_id'])
+
+        await member.remove_roles(guild.get_role(786884615192313866))
+        data['reminded'] = True
+        await self.bot.votes.upsert(data)
+        try:
+            await member.send("You can now vote for The Gambler's Kingdom again!", view=view)
+        except discord.HTTPException:
+            pass
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.guild or message.guild.id != 785839283847954433: return
+        if not message.author.bot: return
+
+        if message.author.id != 270904126974590976: return
+        if not len(message.embeds) > 0: return
+        embed = message.embeds[0]
+
+        if not embed.description.startswith("Successfully paid") and not embed.description.startswith("from the server's pool!"): return
+        command_message = await message.channel.fetch_message(message.reference.message_id)
+        if command_message.interaction is None: return
+        if command_message.interaction.name != "serverevents payout": return
+
+        embed = command_message.embeds[0].to_dict()
+        winner = re.findall(r"<@!?\d+>", embed['description'])
+        prize = re.findall(r"\*\*(.*?)\*\*", embed['description'])[0]
+        emojis = list(set(re.findall(":\w*:\d*", prize)))
+        for emoji in emojis :prize = prize.replace(emoji,"",100); prize = prize.replace("<>","",100);prize = prize.replace("<a>","",100);prize = prize.replace("  "," ",100)
+
+        log_embed = discord.Embed(title="Server Events Payout", description=f"",color=self.bot.default_color)
+        log_embed.description += f"**Winner**: {winner[0]}\n"
+        log_embed.description += f"**Prize**: {prize}\n"
+        log_embed.description += f"**Paid by**: {command_message.interaction.user.mention}\n"
+        link_view = discord.ui.View()
+        link_view.add_item(discord.ui.Button(label="Go to Payout Message", url=command_message.jump_url))
+        log_channel = self.bot.get_channel(1076586539368333342)
+        await log_channel.send(embed=log_embed, view=link_view)
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            return
+
+        elif isinstance(error, commands.CommandOnCooldown):
+            return await ctx.send(f"This command is on cooldown for {error.retry_after:.2f} seconds")
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            return await ctx.send(f"Missing required argument {error.param.name}")
+
+        elif isinstance(error, commands.BadArgument):
+            return await ctx.send(f"Bad argument {error.param.name}")
+
+        elif isinstance(error, commands.MissingPermissions):
+            return await ctx.send(f"You don't have permission to use this command")
+
+        elif isinstance(error, commands.BotMissingPermissions):
+            return await ctx.send(f"I don't have permission to use this command")
+
+        elif isinstance(error, commands.CheckFailure):
+            return await ctx.send(f"You don't have permission to use this command")
+
+        elif isinstance(error, commands.CommandInvokeError):
+            return await ctx.send(f"An error occured while executing this command\n```\n{error}\n```")
+
+        else:
+            embed = discord.Embed(color=0xE74C3C,description=f"<:dnd:840490624670892063> | Error: `{error}`")
+            await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Events(bot))
