532c18477f80ca34c854c6f1ef76f45c98cac5a4
Jay Patel <79038529+Jay24004@users.noreply.github.com>
2024-01-19 12:35:45 +0530

    Update linked_roles.py

--- cogs/linked_roles.py
+++ cogs/linked_roles.py
@@ -106,7 +106,8 @@ class Linked_Roles(commands.GroupCog, name="linkedroles"):
 	@app_commands.command(name="update", description="Update your linked roles")
 	@app_commands.default_permissions(manage_guild=True)
 	@app_commands.describe(user="The user you want to update the roles for", link="The link you want to update", value="The value you want to set the link to")
-	@app_commands.choices(link=[app_commands.Choice(name="♡੭𓈒 Beast Donor ♡੭𓈒", value="top_dono"), app_commands.Choice(name="࿔･ﾟ♛ Weekly Topper ♛ ࿔･ﾟ", value="weekly_champ"), app_commands.Choice(name="Ex-Staff of TGK", value="ex_staff")])
+	@app_commands.choices(link=[app_commands.Choice(name="Beast Donor", value="top_dono"), app_commands.Choice(name="Weekly Topper", value="weekly_champ"), app_commands.Choice(name="Former Staff", value="ex_staff"),
+				   app_commands.Choice(name="New Year Champions", value="new_year_champ"])
 	async def _update(self, interaction: Interaction, user: discord.Member, link: app_commands.Choice[str], value: bool=False):
 		data = await self.bot.auth.find({'discordId': str(user.id)})
 		if data is None:
