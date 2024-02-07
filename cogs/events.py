fd744c7a560483919fc1870e87b38d7797f6903e
Jay Patel <79038529+Jay24004@users.noreply.github.com>
2024-02-07 14:40:23 +0530

    add stickey message for bump

--- cogs/events.py
+++ cogs/events.py
@@ -177,10 +177,12 @@ class Events(commands.Cog):
             if embed.description.startswith("**Thanks for bumping"):
                 await message.reply("Thanks for bumping the server")
                 await message.channel.set_permissions(message.guild.default_role, send_messages=False)
+                await message.channel.send("While you wait for next bump, vote us at https://disurl.me/server/785839283847954433/vote")
 
             if 'You cant bump' in embed.description:
                 await message.reply("You can't bump the server yet")
                 await message.channel.set_permissions(message.guild.default_role, send_messages=False)
+                await message.channel.send("While you wait for next bump, vote us at https://disurl.me/server/785839283847954433/vote")
         
     
     @commands.Cog.listener()
